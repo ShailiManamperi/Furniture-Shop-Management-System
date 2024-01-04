ff3167772aa25cfeb373dde4518b969a2585d283
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-01-04 14:06:40 +0530

    adding Dto,Dtm ,Entity ,to

---
+++ src/main/java/lk/ijse/shaili/system/Dtm/ItemTM.java
@@ -0,0 +1,37 @@
+package lk.ijse.shaili.system.Dtm;
+
+public class ItemTM {
+    private String code;
+    private String name;
+    private int qty;
+
+    public ItemTM(String code, String name, int qty) {
+        this.code = code;
+        this.name = name;
+        this.qty = qty;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+}
---
+++ src/main/java/lk/ijse/shaili/system/Entity/Order.java
@@ -0,0 +1,77 @@
+package lk.ijse.shaili.system.Entity;
+
+public class Order implements SuperEntity{
+    private String oid;
+    private String date;
+    private String cid;
+    private String status;
+    private double price;
+
+    public Order(String oid, String date, String cid, String status, double price) {
+        this.oid = oid;
+        this.date = date;
+        this.cid = cid;
+        this.status = status;
+        this.price = price;
+    }
+    public Order (String date,String oid,double price,String status){
+        this.date=date;
+        this.oid=oid;
+        this.price=price;
+        this.status=status;
+    }
+
+    public Order() {
+    }
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "oid='" + oid + '\'' +
+                ", date='" + date + '\'' +
+                ", cid='" + cid + '\'' +
+                ", status='" + status + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
